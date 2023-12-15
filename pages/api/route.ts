b0f071c7864febd01a41b9533ca05f47826d55b8
nicoxroll <crespo-nicolas@hotmail.com>
2023-12-15 13:41:56 -0300

    region

--- pages/api/route.ts
+++ pages/api/route.ts
@@ -1,3 +1,14 @@
+
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+ 
+export const config = {
+  runtime: 'edge', // this is a pre-requisite
+  // execute this function on iad1 or hnd1, based on the connecting client location
+  regions: ['iad1', 'hnd1'],
+};
+
 export const maxDuration = 40; // This function can run for a maximum of 40 seconds
 export const dynamic = 'force-dynamic';
  
@@ -5,4 +16,11 @@ export function GET(request: Request) {
   return new Response('Vercel', {
     status: 200,
   });
+}
+ 
+export default function handler(request: NextRequest) {
+  return NextResponse.json({
+    name: `I am an Edge Function! (executed on ${process.env.VERCEL_REGION})`,
+  });
+
 }
